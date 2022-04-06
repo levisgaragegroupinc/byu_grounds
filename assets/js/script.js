2b144daee51b2b6ac6d934a28ce9573b1b4479ba
Travis Levi B <tbateswsu@gmail.com>
2022-04-05 17:05:05 -0700

    added styling, buttons, text field, mement.js, autofill dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+
+
+var secondInterval = window.setInterval(function() {
+    var currentDateTime = moment();
+    $('#currentDay').text(currentDateTime.format('ddd, h:mm a'));
+
+}, 1000);
+
+var subNum = 0
+
+for (let num = 0; num < 3; ++num) {
+    var dayNew = moment().add(subNum, 'days');
+    var day = '#day';
+    let dayId = day + num;
+    console.log(dayId);
+    $(dayId).text(dayNew.format('ddd'));
+    subNum = subNum -1;
+    console.log(subNum);
+};
+
+
 // BYU_Grounds-Haws_Field 
 // Token Token 0a86cfd4669882c8a0a5165f0ef6609a8794964c
 
@@ -19,9 +40,9 @@
 // CURL
 // curl -X GET "https://zentracloud.com/api/v3/get_env_model_data/?device_sn=z6-12691&model_type=ETo&port_num=6&inputs=%7B%22elevation%22%3A%201400.4%2C%20%22latitude%22%3A%2040.2466394%2C%20%22wind_measurement_height%22%3A%202%7D" -H  "accept: application/json" -H  "Authorization: Token 0a86cfd4669882c8a0a5165f0ef6609a8794964c"
 
-const urlName = 'https://zentracloud.com/api/v3/get_env_model_data/?device_sn=z6-12691&model_type=ETo&port_num=6&inputs=%7B%22elevation%22%3A%201400.4%2C%20%22latitude%22%3A%2040.2466394%2C%20%22wind_measurement_height%22%3A%202%7D';
- const header1 = 'accept: application/json';
- const header2 = 'Authorization: Token 0a86cfd4669882c8a0a5165f0ef6609a8794964c';
+// const urlName = 'https://zentracloud.com/api/v3/get_env_model_data/?device_sn=z6-12691&model_type=ETo&port_num=6&inputs=%7B%22elevation%22%3A%201400.4%2C%20%22latitude%22%3A%2040.2466394%2C%20%22wind_measurement_height%22%3A%202%7D';
+//  const header1 = 'accept: application/json';
+//  const header2 = 'Authorization: Token 0a86cfd4669882c8a0a5165f0ef6609a8794964c';
 
 // Request URL
 // https://zentracloud.com/api/v3/get_env_model_data/?device_sn=z6-12691&model_type=ETo&port_num=6&inputs=%7B%22elevation%22%3A%201400.4%2C%20%22latitude%22%3A%2040.2466394%2C%20%22wind_measurement_height%22%3A%202%7D
@@ -42,3 +63,50 @@ const urlName = 'https://zentracloud.com/api/v3/get_env_model_data/?device_sn=z6
 //         }
 //     }
         
+
+// const serialNumber1 = 'z6-12691';
+// const serialNumber2 = 'z6-12677';
+
+// var url = 'https://zentracloud.com/api/v3/get_readings/?device_sn=z6-12691&start_date=04-04-2022%2000%3A00&end_date=04-04-2022%2023%3A59&output_format=json&page_num=1&per_page=1000&sort_by=descending'
+
+
+
+// Access to XMLHttpRequest at 'https://zentracloud.com/api/v3/get_readings/?device_sn=z6-12691&start_date=04-04-2022%2000%3A00&end_date=04-04-2022%2023%3A59&output_format=json&page_num=1&per_page=1000&sort_by=descending' 
+// from origin 'null' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
+// Example of fetch with headers
+// fetch('https://reqbin.com/echo/get/json', {
+//   headers: {Authentication: 'Bearer Token'}
+// })
+//    .then(resp => resp.json())
+//    .then(json => console.log(json))
+
+
+// // Example of fetch with custom headers   
+//    fetch('https://reqbin.com/echo/get/json', {
+//     headers: {
+//       Accept: 'application/json',
+//       Authentication: 'Bearer Token',
+//       'X-Custom-Header': 'header value'
+//     }
+//   })
+//      .then(resp => resp.json())
+//      .then(json => console.log(json))
+
+// Using the ZENTRA API
+// fetch('https://zentracloud.com/api/v3/get_readings/?device_sn=z6-12691&start_date=04-04-2022%2000%3A00&end_date=04-04-2022%2023%3A59&output_format=json&page_num=1&per_page=1000&sort_by=descending', 
+// {   mode: 'no-cors',
+//     headers: {
+//       Accept: 'application/json',
+//       Authentication: 'Token d2ae37dcd2a29e5b4785e16ddaa598bfeccf9e09',
+//     }
+//   }).then(resp => resp.json())
+//     .then(json => console.log(json));
+
+// Using the ZENTRA API CSV format
+// https://zentracloud.com/api/v1/download?sn=z6-12691&device_password=36127-87571&response_type=csv
+
+// fetch('https://zentracloud.com/api/v1/download?sn=z6-12691&device_password=36127-87571&response_type=csv',
+// { mode: 'no-cors'})
+//     .then(resp => resp.text())
+//     .then(text => console.log(text));
